d8f6aee3637a3fa64fe431e49c047c69799c6315
Nagamani Kota <reachouttonaga@gmail.com>
2018-07-30 21:45:47 -0400

    Testing....

---
+++ org-info/src/OrganizationDetails/OrganizationDetails.js
@@ -0,0 +1,23 @@
+import React,{Component} from 'react';
+import ReactTable from "react-table";
+
+
+class OrganizationDetails extends React.Component
+{
+
+    render(){
+        return(
+            <div>
+                Hello
+                </div>
+        );
+    }
+
+
+      
+
+}
+
+
+
+export default OrganizationDetails;
